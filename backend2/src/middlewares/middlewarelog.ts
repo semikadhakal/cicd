0a7f96f98ded1c3974d6e3455f1523648b0c32f6
semikadhakal <semikadhakal1@gmail.com>
2025-07-07 11:20:53 +0545

    first commit

---
+++ backend2/src/middlewares/middlewarelog.ts
@@ -0,0 +1,22 @@
+import { logger } from "../utils/logger"
+import { NextFunction, Request, Response } from 'express';
+
+export const middlewarelog=(req:Request,res:Response,next:NextFunction)=>{
+const start = Date.now();
+
+  res.on("finish", () => {
+    const { method, originalUrl } = req;
+    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const userAgent = req.headers["user-agent"];
+    const status = res.statusCode;
+    const duration = Date.now() - start;
+
+    logger.info(
+      `${ip} - - [${new Date().toISOString()}] "${method} ${originalUrl}" ${status} - "${userAgent}" ${duration}ms`
+      );
+      
+      
+  });
+
+  next();
+}
